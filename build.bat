5759d6a7470a6226a0f9c0f21867f0f7f53cfe8d
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-02-21 19:59:52 +0100

    Disable generating tests and examples

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cmake -B build -G"Visual Studio 16" -DNO_WEBSOCKET=1
+cmake -B build -G"Visual Studio 16" -DNO_WEBSOCKET=1 -DNO_TESTS=1 -DNO_EXAMPLES=1
 cmake --build build --target datachannel-static --config Release
